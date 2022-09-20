44f895cc8511a9031654cd62ac7d60e0411729b7
Luke Kvamme <Luke.govols@me.com>
2022-09-19 20:12:17 -0400

    Update Player.java
    Saved Player.java

--- GitHubPresentation/src/DiceGame/Player.java
+++ GitHubPresentation/src/DiceGame/Player.java
@@ -1,5 +1,28 @@
 package DiceGame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
+	
+	private static List<Dice> dice = new ArrayList<Dice>();
+	
+	
+	public void addDice(Dice die) {
+		dice.add(die);
+	}
+	
+	/**
+	 * phillip
+	 */
+	public void rollDice() {
+		ArrayList<Integer> rolls;
+		
+		for (Dice die : dice) {
+			rolls.add(die.roll());
+		}
+		
+		return rolls;
+	}
 
 }
