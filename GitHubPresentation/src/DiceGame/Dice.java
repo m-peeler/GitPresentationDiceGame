973f96abd05bdbe278acc86492da73846d3bd8aa
m-peeler <mdavispeeler@gmail.com>
2022-09-19 21:21:12 -0400

    Added error for presenter

--- GitHubPresentation/src/DiceGame/Dice.java
+++ GitHubPresentation/src/DiceGame/Dice.java
@@ -8,12 +8,16 @@ import java.util.List;
  *
  */
 public interface Dice {
-
-	public abstract int roll();
 	
-	public abstract List<Integer> rollNDice(int n);
+	/**
+	 * TODO: Presenter
+	 */
+	public abstract int roll()
 	
-	public abstract int numSides();
+	public abstract List<Integer> rollNDice(int n)
 	
-	public abstract List<Integer> sideValues();
+	public abstract int numSides()
+	
+	public abstract List<Integer> sideValues()
+	
 }
